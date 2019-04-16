20b5bc9bcbd0e85b64b32845cca9ff9f17d3b09b
Nilay Savant <nilayofficial@gmail.com>
2019-04-16 19:54:53 +0530

    Code refactored

--- a.js
+++ a.js
@@ -9,10 +9,31 @@
 
 // ball object
 const ball = {
-    x: 0,
-    y: 0,
-    speed: 10,
-    dia: 50,
+    // Attributes of ball
+    x: 0, // x cord
+    y: 0, // y cord
+    speed: 10, // move speed
+    dia: 50, // diamenter
+
+    // display the ball object
+    display: () => {
+        circle(ball.x, ball.y, ball.dia)
+        fill(0)
+    },
+
+    // movement functions
+    ballUp: () => {
+        ball.y -= ball.speed
+    },
+    ballDown: () => {
+        ball.y += ball.speed
+    },
+    ballLeft: () => {
+        ball.x -= ball.speed
+    },
+    ballRight: () => {
+        ball.x += ball.speed
+    },
 }
 
 // SETUP -----------------------------------------<<<
@@ -27,7 +48,7 @@ function setup() {
 function draw() {
     clear()
     initCanvas()
-    displayBall()
+    ball.display()
     keyboardInput()
 }
 
@@ -41,49 +62,25 @@ function initCanvas() {
     frameRate(60)
 }
 
+
 /**
- * Display a ball 
+ *  add gravity to ball
  */
-function displayBall() {
-    console.log('display ball')
-    circle(ball.x, ball.y, ball.dia)
-    fill(0)
-}
+function addGravity() {
 
-/*
-    A fucntions to move the ball
-*/
-function ballDown() {
-    console.log('ball down')
-    ball.y += ball.speed
-}
-
-function ballUp() {
-    console.log('ball up')
-    ball.y -= ball.speed
-}
-
-function ballLeft() {
-    console.log('ball left')
-    ball.x -= ball.speed
-}
-
-function ballRight() {
-    console.log('ball right')
-    ball.x += ball.speed
 }
 
 // KeyPress function
 function keyboardInput() {
     console.log('keyboard input', keyCode)
     if (keyIsDown(87)) { // W prees: UP
-        ballUp()
+        ball.ballUp()
     } else if (keyIsDown(83)) { // S press: DOWN
-        ballDown()
+        ball.ballDown()
     } else if (keyIsDown(65)) { // A press: LEFT
-        ballLeft()
+        ball.ballLeft()
     } else if (keyIsDown(68)) { // D press: RIGHT
-        ballRight()
+        ball.ballRight()
     }
     return false; // prevent default
 }
